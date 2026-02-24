d553c09bc7066c8152e389c83bab74286b023cf5
nibirshahriar <nibir1891@gmail.com>
2026-02-24 16:20:16 +0600

    added eventListener

--- script/script.js
+++ script/script.js
@@ -39,3 +39,77 @@ function toggleStyle(id) {
   selectedBtn.classList.remove("bg-gray-200", "text-black");
   selectedBtn.classList.add("btn-primary", "text-white");
 }
+
+mainContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("interview-btn")) {
+    const parentNode = event.target.closest(".jcard");
+
+    const jobName = parentNode.querySelector(".jobName").innerText;
+    const infoName = parentNode.querySelector(".infoName").innerText;
+    const remote = parentNode.querySelector(".remote").innerText;
+    const notes = parentNode.querySelector(".notes").innerText;
+
+    parentNode.querySelector(".state").innerText = "INTERVIEW";
+    parentNode.querySelector(".state").classList.remove("bg-blue-50");
+    parentNode
+      .querySelector(".state")
+      .classList.add("bg-green-100", "text-green-700");
+
+    const cardInfo = {
+      jobName,
+      infoName,
+      remote,
+      state: "INTERVIEW",
+      notes,
+    };
+
+    const jobExist = interviewList.find(
+      (item) => item.jobName == cardInfo.jobName,
+    );
+
+    if (!jobExist) {
+      interviewList.push(cardInfo);
+    }
+
+    rejectedList = rejectedList.filter(
+      (item) => item.jobName != cardInfo.jobName,
+    );
+
+    calculateCount();
+  } else if (event.target.classList.contains("Rejected-btn")) {
+    const parentNode = event.target.closest(".jcard");
+
+    const jobName = parentNode.querySelector(".jobName").innerText;
+    const infoName = parentNode.querySelector(".infoName").innerText;
+    const remote = parentNode.querySelector(".remote").innerText;
+    const notes = parentNode.querySelector(".notes").innerText;
+
+    parentNode.querySelector(".state").innerText = "REJECTED";
+    parentNode.querySelector(".state").classList.remove("bg-blue-50");
+    parentNode
+      .querySelector(".state")
+      .classList.add("bg-red-100", "text-red-700");
+
+    const cardInfo = {
+      jobName,
+      infoName,
+      remote,
+      state: "REJECTED",
+      notes,
+    };
+
+    const jobExist = rejectedList.find(
+      (item) => item.jobName == cardInfo.jobName,
+    );
+
+    if (!jobExist) {
+      rejectedList.push(cardInfo);
+    }
+
+    interviewList = interviewList.filter(
+      (item) => item.jobName != cardInfo.jobName,
+    );
+
+    calculateCount();
+  }
+});
