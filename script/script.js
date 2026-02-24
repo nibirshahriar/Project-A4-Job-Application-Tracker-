5895a7f7256ac5adb62e4cae8b02a2e3d7156af8
nibirshahriar <nibir1891@gmail.com>
2026-02-24 12:33:53 +0600

    added total,interview and rejected count

--- script/script.js
+++ script/script.js
@@ -1,1 +1,17 @@
- 
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("total");
+let interview = document.getElementById("interviewCount");
+let rejected = document.getElementById("rejectedCount");
+let jobs = document.getElementById("jobs");
+
+const allCards = document.getElementById("job-cards");
+
+function calculateCount() {
+  total.innerText = allCards.children.length;
+  interview.innerText = interviewList.length;
+  rejected.innerText = rejectedList.length;
+}
+
+calculateCount();
